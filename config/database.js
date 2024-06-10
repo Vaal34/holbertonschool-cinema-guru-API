9ce803f48c1bd54195874615516e779183771a58
Vaal34 <5619@holbertonstudents.com>
2024-06-10 15:28:00 +0200

    Setting up the workspace

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize').Sequelize;
 const sequelize = new Sequelize(
     process.env.DB_SCHEMA || 'postgres',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || '',
+    process.env.DB_USER || 'cinema',
+    process.env.DB_PASSWORD || 'root',
     {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 5432,
